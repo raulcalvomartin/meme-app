a0604da4f3f21481da818690f28e5b7c9fafcb96
Raul Calvo Martin <raul.calvo.martin@citrix.com>
2021-07-30 22:50:44 +0200

    Initial Commit

---
+++ meme-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  meme-app
+//
+//  Created by raul_builder on 30.07.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
